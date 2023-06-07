79c8f1bd5a6afa2f65f45d3024aaccfe603702eb
João Martins <joao.martins@talkdesk.com>
2023-06-07 15:02:51 +0100

    Fix SQL injection vulnerability in product search

--- src/main/java/nl/brianvermeer/workshop/coffee/repository/SearchRepository.java
+++ src/main/java/nl/brianvermeer/workshop/coffee/repository/SearchRepository.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.sql.DataSource;
 import java.util.List;
 import java.util.Locale;
@@ -20,9 +21,14 @@ public class SearchRepository {
 
     public List<Product> searchProduct (String input) {
         var lowerInput = input.toLowerCase(Locale.ROOT);
-        String query = "Select * from Product where lower(description) like '%" + lowerInput + "%' OR lower(product_name) like '%" + lowerInput + "%'";
-        var resultList = (List<Product>) em.createNativeQuery(query, Product.class).getResultList();
-        return resultList;
+        String queryText = "SELECT * FROM Product " +
+                           "WHERE lower(description) LIKE CONCAT('%', ?1, '%') OR " +
+                           "      lower(product_name) LIKE CONCAT('%', ?2, '%')";
+
+        var query = em.createNativeQuery(queryText, Product.class);
+        query.setParameter(1, lowerInput);
+        query.setParameter(2, lowerInput);
+        return (List<Product>) query.getResultList();
     }
 
 }
